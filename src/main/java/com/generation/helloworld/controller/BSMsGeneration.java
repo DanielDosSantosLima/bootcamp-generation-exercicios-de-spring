0348e5ab2a1cbecb7513ac1f165c88ae3d0ee732
Daniel Lima <dsl.daniel.santos.lima@gmail.com>
2024-11-26 21:47:47 -0300

    Exercícios de Spring

---
+++ src/main/java/com/generation/helloworld/controller/BSMsGeneration.java
@@ -0,0 +1,17 @@
+package com.generation.helloworld.controller;
+
+public class BSMsGeneration {
+
+	public String MentalidadesGen() {
+	return	"BSM's - Mentalidades da Generation" +
+			"Persistência" +
+			"Mentalidade de Crescimento" +
+			"Orientação ao Futuro" +
+			"Responsabilidade Pessoal" +
+			"Trabalho em Equipe" +
+			"Comunicação" +
+			"Comunicação Não Violenta" +
+			"Atenção para Detalhes" +
+			"Proatividade";
+	}
+}
---
+++ src/main/java/com/generation/helloworld/controller/ObjetivosDeAprendizagem.java
@@ -0,0 +1,11 @@
+package com.generation.helloworld.controller;
+
+public class ObjetivosDeAprendizagem {
+
+	public String ObjetivosDaSemana() {
+		return	"Objetivos de Aprendizagem da Semana:" +
+				"Aprender mais sobre Back-End" +
+				"Entender melhor o que é uma API" +
+				"Explorar melhor a relação entre Back-End e Banco de Dados";
+		}
+}
